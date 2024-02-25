bdff5efc1f49edc4d804b44a3f301564c19fcb99
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-25 23:54:00 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -12,6 +12,7 @@ Thanks Too :
 > Xyroinee
 > Amelia Aubert (Story)
 > Clara Aubert (Support)
+> Iky Official (Recode)
 > Clayza Aubert (Recode)
 
 Script Victoria Rosalind Di Recode Oleh: *Clayza Aubert*
@@ -24,7 +25,7 @@ conn.sendMessage(m.chat, {
 	title: 'Credit Bot Whatsapp',
 	body: 'Jangan Di Hapus Atau ERROR',
 	thumbnailUrl: global.thumb,
-	sourceUrl: 'https://youtube.com/c/ClayzaAubert',
+	sourceUrl: 'https://whatsapp.com/channel/0029VaACVbGDuMRk7zy6vI3a',
 	mediaType: 1,
 	renderLargerThumbnail: true
 	}}})
